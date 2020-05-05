69b15031ed2f9425c3009378f168ec1a7fb2651c
Azim M <azim@Manjee.consulting>
2020-05-05 13:55:23 -0400

    Add files via upload

---
+++ installmodule-allautomationaccts.ps1
@@ -0,0 +1,15 @@
+﻿$subs = Get-AzSubscription
+foreach ($sub in $subs) {
+    
+    $id = $sub.id
+    Set-AzContext -SubscriptionId $id
+    $autoaccts = Get-AzAutomationAccount
+    foreach ($autoacct in $autoaccts ) {
+        
+        $acct = $acct.AutomationAccountName
+        $rg = $acct.ResourceGroupName 
+        New-AzAutomationModule -AutomationAccountName $acct -ResourceGroupName $rg -Name "Microsoft.RDInfra.RDPowershell" -ContentLinkUri "https://www.powershellgallery.com/api/v2/package/Microsoft.RDInfra.RDPowershell/1.0.1.7"
+        
+    } #automationacct loop
+    
+} #subs loop

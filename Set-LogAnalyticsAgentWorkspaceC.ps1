36b062cf39989c538355d46f03cd423f3fab9774
Azim M <azmanjee@Microsoft.com>
2021-10-04 16:35:28 -0400

    Create Set-LogAnalyticsAgentWorkspaceC.ps1

---
+++ Set-LogAnalyticsAgentWorkspaceC.ps1
@@ -0,0 +1,35 @@
+<#
+.SYNOPSIS
+    This function sets the Log Analytics Workspace locally on the machine byt reconfiguring the Agent Locally
+.DESCRIPTION
+    This will invoke a coommand on a PC remotely to set the Log Analytics Workspace on a machine using PSRemoting/WSMan
+.EXAMPLE
+    Set-LogAnalyticsAgentWorkspaceC.ps1 -Workspaceid '<WORKSPACE ID GOES HERE>' -WorkspaceKey '<WORKSPACE KEY GOES HERE>' -Computername SERVER1
+.PARAMETER Workspaceid
+    This is the Workspace ID for the cmdlet
+.PARAMETER Workspacekey
+    Workspace key for your Workspace
+.PARAMETER Computername
+    Target Computer
+.OUTPUTS
+    Output from this cmdlet (if any)
+.NOTES
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#>
+Function Set-LogAnalyticsAgentWorkspaceConfiguration {
+	[CmdletBinding()]
+	param(
+        [Parameter(Mandatory)]
+        [string]$Workspaceid,
+        [string]$Workspacekey,
+        [string]$Computername = 'localhost'
+        )
+    foreach ($name in $Computername) {
+            Invoke-Command -ComputerName $name -ScriptBlock {
+                $mma = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
+                $mma.AddCloudWorkspace($WorkspaceId, $WorkspaceKey)
+                $mma.ReloadConfiguration()
+            }
+        }
+}

38f4c243c7283e409127d4ac5ce0e62847b7ba00
Azim M <azmanjee@Microsoft.com>
2021-11-26 15:52:32 -0500

    Create PR-Choco-Apps.ps1

---
+++ ProactiveRemediations/PR-Choco-Apps.ps1
@@ -0,0 +1,50 @@
+$mode = $MyInvocation.MyCommand.Name.Split(".")[0]
+$modechange = $false
+#checkforapps
+$apptolookfor = "powertoys","prusaslicer","firefox","googlechrome","7zip","chrome-remote-desktop-host","vscode","vlc","python3","microsoft-windows-terminal","winscp","git","rsat","steam-client","microsoft-windows-terminal","notepadplusplus","azcopy","obs-studio","obs-virtualcam","obs-ndi","firefox","rufus","vscode-python","vscode-arduino","vscode-powershell","drawio","discord","microsoftazurestorageexplorer"
+
+[System.Collections.ArrayList]$chocoapps = (choco list --local-only).split([Environment]::NewLine)
+[System.Collections.ArrayList]$chocooutdatedapps = (choco outdated).split([Environment]::NewLine)
+$chocoversion = $chocoapps[0]
+$chocopkgcountremoval = $chocoapps[-1]
+$chocoapps.remove($chocopkgcountremoval)
+$chocoapps.remove($chocoversion)
+
+if($mode -eq "detect") {
+    foreach ($app in $apptolookfor){
+        $appexists = ($apps -contains "$app")
+        if ($appexists -eq $true) {
+            write-host "$app found"
+        }
+        Else {
+            write-host "$app not found, marking for remediation"
+            $modechange = $true
+        }
+    }
+    if ($chocooutdatedapps -ne $null) {
+        write-host "outdated apps detected"
+        $modechange = $true
+    }
+    if ($modechange -eq $true){exit 1}
+    elsif ($modechange -eq $false){exit 0}
+}
+Elseif($mode -ne "detect" -or $null) {
+    foreach ($app in $apptolookfor){
+        $appexists = ($apps -contains "$app")
+        if ($appexists -eq $true) {
+            write-host "$app found"
+        }
+        Else {
+            write-host "$app not found, installing $app"
+            choco install $app -y
+        }
+        if ($chocooutdatedapps -ne $null) {
+            write-host "outdated apps detected"
+            choco upgrade all
+        }
+    }
+
+}
+elseif ($mode -eq $null){
+    Write-host "Not in Proactive Remedations context!"
+}

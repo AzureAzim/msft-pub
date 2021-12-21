35367b6f457d37d8e6b855e1a76834589333074c
Azim M <azmanjee@Microsoft.com>
2021-12-20 23:24:56 -0500

    Update Remediation-Baseline-Template.ps1
    fixed exit strategy,  completed modechange logic

--- ProactiveRemediations/Remediation-Baseline-Template.ps1
+++ ProactiveRemediations/Remediation-Baseline-Template.ps1
@@ -5,13 +5,20 @@ $modechange = $false
 ##detection mode
 if($mode -eq "detect") {
 
+if(#we detect the tthing we wanna change
+) {
+
+    $modechange = $true
+}
+
+    if ($modechange -eq $false){exit 0}
+    if ($modechange -eq $true){$modeerror; exit 1}
 
 }
 ##remediation mode TODO: Get actual mode string from a runtime
-Elseif($mode -ne "detect" -or $null) {
+Elseif($mode --eq "remediate") {
 
-}
-##Testing mode? 
-elseif ($mode -eq $null){
-    Write-host "Not in Proactive Remedations context!"
+
+
+exit 0
 }
